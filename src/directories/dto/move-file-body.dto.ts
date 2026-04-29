ac867ad76c6ed13374f71b966b18a910a95d3626
Kiril Yakymchuk <stbestich@gmail.com>
2026-04-29 20:19:56 +0300

    fix: broken target dir on move validation

--- src/directories/dto/move-file-body.dto.ts
+++ src/directories/dto/move-file-body.dto.ts
@@ -1,11 +1,9 @@
-import { IsNotEmpty, IsString, IsUUID, ValidateIf } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class MoveFileBodyDto {
   @IsString()
   @IsNotEmpty()
-  @ValidateIf((object: MoveFileBodyDto) => object.targetDir !== 'root')
-  @IsUUID()
   @ApiProperty({
     example: '0197d0ae-ab4a-7bf3-a32e-4fe889c2e2db',
     description: 'A valid UUID or the keyword "root"',
